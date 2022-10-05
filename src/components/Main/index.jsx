1010ba5ce3c8ed2d042ad478939a896be24e69fc
Sidney Cardoso <sidneyrsc342@gmail.com>
2022-10-04 23:53:02 -0300

    Editando e deletando tarefas

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -9,16 +9,56 @@ export default class Main extends Component {
 
     state = {
         newTask: '',
-        tasks: [
-            'Limpar a casa',
-            'Estudar',
-            'trabalhar'
-        ]
+        tasks: [],
+        index: -1
     }
 
+    handleSubmit = e => {
+        e.preventDefault()
+        const { tasks, index } = this.state
+        let { newTask } = this.state
+        newTask = newTask.trim()
+
+
+        if (tasks.indexOf(newTask) !== -1) return
+
+        const newTasks = [...tasks]
+
+        if (index === -1) {
+            this.setState({
+                tasks: [...newTasks, newTask],
+                newTask: ''
+            })
+        } else {
+            newTasks[index] = newTask
+
+            this.setState({
+                tasks: [...newTasks],
+                index: -1
+            })
+        }
+
+
+    }
     handleChange = e => {
         this.setState({
             newTask: e.target.value
+        })
+    }
+    handleEdit = (e, index) => {
+        const { tasks } = this.state
+        this.setState({
+            index,
+            newTask: tasks[index]
+        })
+    }
+    handleDelete = (e, index) => {
+        const { tasks } = this.state
+        const newTasks = [...tasks]
+        newTasks.splice(index, 1)
+
+        this.setState({
+            tasks: [...newTasks]
         })
     }
     render() {
@@ -26,7 +66,7 @@ export default class Main extends Component {
         return (
             <div className="main">
                 <h1>Lista de tarefas</h1>
-                <form action="#" className="form">
+                <form action="#" className="form" onSubmit={this.handleSubmit}>
                     <input onChange={this.handleChange} type="text" value={newTask} />
                     <button type="submit">
                         <FaPlus />
@@ -34,15 +74,22 @@ export default class Main extends Component {
                 </form>
 
                 <ul className="tasks">
-                    {tasks.map(task => {
+                    {tasks.map((task, index) => {
                         return (
 
                             <li key={task}>
                                 {task}
-                                <div>
-                                    <FaEdit className="edit" />
-                                    <FaWindowClose className="delete" />
-                                </div>
+                                <span>
+                                    <FaEdit
+                                        className="edit"
+                                        onClick={e => this.handleEdit(e, index)}
+                                    />
+                                    <FaWindowClose
+                                        className="delete"
+                                        onClick={e => this.handleDelete(e, index)}
+                                    />
+
+                                </span>
                             </li>
                         )
                     })}
